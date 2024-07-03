c8d91eba142e223d66efcc99e06a26d32cccf2e8
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-07-03 18:42:14 +0530

    Implement TabView setup and add resources.

--- BBQuotes-SwiftUI-iOS17/ContentView.swift
+++ BBQuotes-SwiftUI-iOS17/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Text("Breaking Bad View")
+                .toolbarBackground(.visible, for: .tabBar) // to show tabview bar when clicked on this tab
+                .tabItem {
+                    //tabItem - to add tabs on tabView
+                    //Label - what to show for current tab
+                    Label("Breaking Bad",
+                          systemImage: "tortoise")
+                }
+            
+            Text("Better Call Saul View")
+                .toolbarBackground(.visible, for: .tabBar)
+                .tabItem {
+                    Label("Better Call Saul",
+                          systemImage: "briefcase")
+                }
         }
-        .padding()
+        .preferredColorScheme(.dark) //this makes app theme dark, inspite of what theme is set on user's iphone 
     }
 }
 
