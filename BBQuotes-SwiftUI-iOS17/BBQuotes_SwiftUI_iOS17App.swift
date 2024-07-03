adb96ecc88c89b7a996e172bfa3bb5b73ff01234
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-07-03 18:20:44 +0530

    Initial Commit

---
+++ BBQuotes-SwiftUI-iOS17/BBQuotes_SwiftUI_iOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotes_SwiftUI_iOS17App.swift
+//  BBQuotes-SwiftUI-iOS17
+//
+//  Created by Mayur Vaity on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotes_SwiftUI_iOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
